367b17ea2bcd2f8aa1c568d7fee9dbb9164d5ced
syifaa019 <144194434+syifaa019@users.noreply.github.com>
2024-05-06 15:07:00 +0700

    Add files via upload

---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="text-center my-4">riwayat kunjungan</h2>
+                <div class="my-3">
+                    <input type="search" class="form-control rounded-5" placeholder="Fiter...">
+                </div>
+                 <div class="my-3 text-muted">menampilkan 1 drat</div>
+                 <table class="table">
+                    <thead>
+                        <tr>
+                            <td>#</td>
+                            <td>NAMA</td>
+                            <td>KEANGGOTAAN</td>
+                            <td>WAKTU</td>
+                            <td>KEPERLUAN</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(visitor,i) in visitors" :key="i">
+                           
+                        </tr>
+                    </tbody>
+                 </table>
+            </div>
+        </div>
+        <NuxtLink to="/pengunjung/tambah">
+            <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">kembali</button>
+        </NuxtLink>
+           
+        
+    </div>
+</template>
+<script setup>
+const supabase = useSupabaseClient()
+
+const visitors = ref([])
+
+const getPengunjung = async () => {
+    const { data, error } = await supabase.from('Pengunjung').select(`*, keanggotaan(*), keperluan(*)`)
+    if(data) visitors.value = data
+}
+
+onMounted(() => {
+    getPengunjung()
+})
+</script>
