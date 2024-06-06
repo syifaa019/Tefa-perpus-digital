4948baf3f8f0dae95e5be59d5e22d6cb341631a1
syifaa019 <144194434+syifaa019@users.noreply.github.com>
2024-06-06 14:01:32 +0700

    Update tambah.vue

--- pages/pengunjung/tambah.vue
+++ pages/pengunjung/tambah.vue
@@ -5,7 +5,7 @@
                 <h2 class="text-center my-4">ISI BUKU KUNJUNGAN</h2>
                 <form @submit.prevent="kirimData">
                     <div class="mb-3">
-                        <input type="text" class="form-control form-control-lg rounded-5" placeholder="NAMA...">
+                        <input v-model="form.nama" type="text" class="form-control form-control-lg rounded-5" placeholder="NAMA...">
                     </div>
                     <div class="mb-3">
                         <select v-model="form.keanggotaan" class="form-control form-control-lg form-select rounded-5 abu">
@@ -14,57 +14,38 @@
                     </select>
                     </div>
                     <div v-if="form.keanggotaan == 2" class="mb-3">
-                        <div class="row">
-                    <!-- <div class="mb-3">
-                        <select class="form-control form-control-lg form-select rounded-5">
-                            <option value="">KEANGGOTAAN</option>
-                            <option value="Siswa">Siswa</option>
-                            <option value="Guru">Guru</option>
-                            <option value="Staf">Staf</option>
-                            <option value="Umum">Umum</option>
-                        </select>
-                    </div> -->
-                    <div class="mb-3"> 
-                        <div class="row">
-                            <div class="col-md-4">
-                                <select class="form-control form-control-lg form-select rounded-5 mb-2">
-                                    <option value="">TINGKAT</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                </select>
-                            </div>
-                            <div class="col-md-4">
-                                <select class="form-control form-control-lg form-select rounded-5 mb-2">
-                                    <option value="">JURUSAN</option>
-                                    <option value="PPLG">PPLG</option>
-                                    <option value="TJKT">TJKT</option>
-                                    <option value="TSM">TSM</option>
-                                    <option value="DKV">DKV</option>
-                                    <option value="TOI">TOI</option>
-                                </select>
-                            </div>
-                            <div class="col-md-4">
-                                <select class="form-control form-control-lg form-select rounded-5 mb-2">
-                                    <option value="">KELAS</option>
-                                    <option value="X">X</option>
-                                    <option value="XI">XI</option>
-                                    <option value="XII">XII</option>
-                                    </select>
                         
-            
-             <Nuxt-Link to="/">
-                <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">KIRIM</button>
-                
-            <button type="submit" class="btn btn-dark  btn-lg rounded-5 px-5"
-                style="margin-left: 82%;">KEMBALI</button>
-            </Nuxt-Link>
-                </form>
-            </div></div>
+                        <div class="mb-3"> 
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <select v-model="form.tingkat" class="form-control form-control-lg form-select rounded-5 mb-2">
+                                        <option value="">TINGKAT</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-4">
+                                    <select v-model="form.jurusan" class="form-control form-control-lg form-select rounded-5 mb-2">
+                                        <option value="">JURUSAN</option>
+                                        <option value="PPLG">PPLG</option>
+                                        <option value="TJKT">TJKT</option>
+                                        <option value="TSM">TSM</option>
+                                        <option value="DKV">DKV</option>
+                                        <option value="TOI">TOI</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-4">
+                                    <select v-model="form.kelas" class="form-control form-control-lg form-select rounded-5 mb-2">
+                                        <option value="">KELAS</option>
+                                        <option value="X">X</option>
+                                        <option value="XI">XI</option>
+                                        <option value="XII">XII</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    </div>
                     </div>
                     <div class="mb-3">
                         <!-- <select class="form-control form-control-lg form-select rounded-5"> -->
@@ -73,6 +54,21 @@
                         <option v-for="(member, i) in objectives" :key="i" :value="member.id">{{ member.nama }}</option>
                         </select>
                     </div>
+                    <div class="row justify-content-evenly">
+                        <div class="col-3">
+                            <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">KIRIM</button>
+                        </div>
+                        <div class="col-3">
+                             <NuxtLink to="/">  
+                            <button type="submit" class="btn btn-dark  btn-lg rounded-5 px-5"
+                            style="margin-left: 82%;">KEMBALI</button>
+                             </NuxtLink>
+                        </div>
+                    </div>
+                    
+                </form>
+            </div>
+                    
         </div>
     </div>
 </template>
